2b3b97866cddf61abdc01eedb5fd21dd99056ba2
Ayaan2907 <ayaanansari7921@outlook.com>
2021-03-06 16:59:38 +0530

    implimented templates, rendered a basic hello template | learned about templates in django | advancement will be done soon

--- trelloApp/views.py
+++ trelloApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# home= {'dic1':'dic', 'dic2':'dic2'}
 def index(request):
     return HttpResponse("Hello")
+
+def home(request):
+    return render(request, "trelloAppTemplates/home.html",{'home': 1234567890})
